93541ebaed5b4b95712f007c046ba71d92223d38
SoizicCistac <79482315+SoizicCistac@users.noreply.github.com>
2021-09-11 22:56:36 +0200

    Update SelectActivity.js

--- src/components/SelectActivity.js
+++ src/components/SelectActivity.js
@@ -20,6 +20,8 @@ const SelectActivity = () =>{
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
+        width: '90%',
+        height: '90%',
       },
     };
 
